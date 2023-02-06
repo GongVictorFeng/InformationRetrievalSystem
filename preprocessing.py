ce416132e49d19d5187fda7d447e2117ef448804
Aaron-Ng-139 <aaronng139@gmail.com>
2023-02-05 20:34:09 -0500

    Preprocessing nearly finished
    Note: testing needed, stemming not implemented yet
    Implemented reading multiple files from directory, separating documents within a file into individual pieces, and processing each document's plaintext into tokens.

--- preprocessing.py
+++ preprocessing.py
@@ -1,26 +1,70 @@
-# Implementing tokenization and stopword removal
-
+############### imports ###############
+import os
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
-sentence = "At eight o'clock on Thursday morning, Arthur didn't feel very good."
+############### variables ###############
+doc_text_pairs = {} # Dictionary of document name to document text pairs
 
-# Tokenization
-tokens = word_tokenize(sentence)
-print(tokens)
+############### functions ###############
 
-# Stopword Removal
-stop_words = set(stopwords.words("english"))
+# Function to separate file into documents
+def separate_docs(f) :
+    while True:
+        line = f.readline()
 
-filtered_list = []
+        if not line:
+            # Stop reading at end of file
+            break
+        elif (line.__contains__("<DOCNO>")) :
+            # Isolate the title of the document
+            doc_title = line[line.index("<DOCNO> ") + 8 : line.index(" </DOCNO>")]
+        elif (line.__contains__("<TEXT>")) :
+            doc_text = ""
+            line = f.readline()
+            while (not line.__contains__("</TEXT>")) :
+                doc_text += line.replace("\n", " ")
+                line = f.readline()
+        elif (line.__contains__("</DOC>")) :
+            # Turn plaintext into tokens (this reduces memory usage by ~20%)
+            tokens = preprocessing(doc_text)
+            # Save the key value pair
+            doc_text_pairs[doc_title] = tokens
+            # Reset values
+            doc_title = ""
+            doc_text = ""
 
-for word in tokens:
-    if word.casefold() not in stop_words:
-        filtered_list.append(word)
+# Function to handle processing of plaintext into vocabulary
+def preprocessing(doc_text) :
+    # Tokenization
+    tokens = word_tokenize(doc_text)
 
-print(filtered_list)
+    # Stopword Removal
+    stop_words = set(stopwords.words("english"))
+    filtered_list = []
+    for word in tokens:
+        if word.casefold() not in stop_words:
+            filtered_list.append(word)
 
-# Remove numbers and punctuation
-filtered_list = [word.lower() for word in filtered_list if word.isalpha()]
+    # Remove numbers and punctuation
+    filtered_list = [word.lower() for word in filtered_list if word.isalpha()]
 
-print(filtered_list)
+    # Return results
+    return filtered_list
+
+############### main ###############
+
+# Get a list of all files in the coll/ directory
+lst_filenames = os.listdir("coll")
+
+# Open each file one by one to separate the documents within the file
+for filename in lst_filenames :
+    file_path =  "coll\\" + filename
+    f = open(file_path, "r")
+    separate_docs(f)
+
+
+# # For testing
+# for a in doc_text_pairs :
+#     print(a, doc_text_pairs[a])
+#     print()
