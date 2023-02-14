c717cc641c8d11cefb36f7c404e52adb1b27799d
Aaron-Ng-139 <aaronng139@gmail.com>
2023-02-14 00:05:42 -0500

    Reformatted preprocessing functions to be more consistent w/ existing code

--- InfoRetrievalSystem.py
+++ InfoRetrievalSystem.py
@@ -10,49 +10,49 @@ index=set() # a set containing all terms, using set to remove duplicates
 ############ fuctions ###############
 
 # Function to separate file into documents
-def separate_docs(f) :
+def separate_docs(f):
     while True:
-        line = f.readline()
+        line=f.readline()
 
         if not line:
             # Stop reading at end of file
             break
-        elif (line.__contains__("<DOCNO>")) :
+        elif (line.__contains__("<DOCNO>")):
             # Isolate the title of the document
-            doc_title = line[line.index("<DOCNO> ") + 8 : line.index(" </DOCNO>")]
-        elif (line.__contains__("<TEXT>")) :
-            doc_text = ""
-            line = f.readline()
-            while (not line.__contains__("</TEXT>")) :
-                doc_text += line.replace("\n", " ")
-                line = f.readline()
-        elif (line.__contains__("</DOC>")) :
+            doc_title=line[line.index("<DOCNO> ")+8:line.index(" </DOCNO>")]
+        elif (line.__contains__("<TEXT>")):
+            doc_text=""
+            line=f.readline()
+            while (not line.__contains__("</TEXT>")):
+                doc_text+=line.replace("\n", " ")
+                line=f.readline()
+        elif (line.__contains__("</DOC>")):
             # Turn plaintext into tokens (this reduces memory usage by ~20%)
-            tokens = preprocessing(doc_text)
+            tokens=preprocessing(doc_text)
             # Save the key value pair
-            doc_dic[doc_title] = tokens
+            doc_dic[doc_title]=tokens
             # Reset values
-            doc_title = ""
-            doc_text = ""
+            doc_title=""
+            doc_text=""
 
 # Function to handle processing of plaintext into vocabulary
-def preprocessing(doc_text) :
+def preprocessing(doc_text):
     # Tokenization
-    tokens = word_tokenize(doc_text)
+    tokens=word_tokenize(doc_text)
 
     # Stopword Removal
-    stop_words = set(stopwords.words("english"))
-    filtered_list = []
+    stop_words=set(stopwords.words("english"))
+    filtered_list=[]
     for word in tokens:
         if word.casefold() not in stop_words:
             filtered_list.append(word)
 
     # Remove numbers and punctuation
-    filtered_list = [word.lower() for word in filtered_list if word.isalpha()]
+    filtered_list=[word.lower() for word in filtered_list if word.isalpha()]
 
     # Stemming w/ Porter Stemmer
-    stemmer = PorterStemmer()
-    filtered_list = [stemmer.stem(word) for word in filtered_list]
+    stemmer=PorterStemmer()
+    filtered_list=[stemmer.stem(word) for word in filtered_list]
 
     # Return results
     index.update(tokens)
