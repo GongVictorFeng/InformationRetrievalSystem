4ffe773f5b518a548fb06a6124071dcb41b1947d
Aaron-Ng-139 <aaronng139@gmail.com>
2023-02-14 00:02:11 -0500

    Vastly improved performance of preprocessing functions

--- InfoRetrievalSystem.py
+++ InfoRetrievalSystem.py
@@ -3,52 +3,60 @@ import math
 import time #progress bar
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
-from nltk.tokenize import RegexpTokenizer
+from nltk.tokenize import RegexpTokenizer, word_tokenize
 ############ variable ##############
 index=set() # a set containing all terms, using set to remove duplicates
 
 ############ fuctions ###############
 
-def createTerms(fileName):
-    """This function is to extract the text from file and tokenize the text, then create a dictionary where the key is the DocNum, the value is the
-        terms in the document; and a index containing terms of all documents.
-        parameter: a file path
-        return: a dictionary."""
+# Function to separate file into documents
+def separate_docs(f) :
+    while True:
+        line = f.readline()
 
-    docs_dic={}
-    infile=open(fileName)
-    docStr=""
-    isText=False
-    for line in infile:
-        if line.__contains__("<DOCNO> "):
-            docNo=line[8:line.index(" </DOCNO>")]
-        if line.__contains__("<TEXT>"):
-            isText=True
-            continue
-        if line.__contains__("</TEXT>"):
-            isText=False
-        if isText:
-            docStr+=line.replace("\n"," ")
-        if line.__contains__("</DOC>"):
-            lowcaseStr=docStr.casefold()
-            tokens=preprocessing(lowcaseStr)
-            index.update(tokens)
-            docs_dic[docNo]=tokens
-            docStr=""
-    infile.close()
-    return docs_dic
+        if not line:
+            # Stop reading at end of file
+            break
+        elif (line.__contains__("<DOCNO>")) :
+            # Isolate the title of the document
+            doc_title = line[line.index("<DOCNO> ") + 8 : line.index(" </DOCNO>")]
+        elif (line.__contains__("<TEXT>")) :
+            doc_text = ""
+            line = f.readline()
+            while (not line.__contains__("</TEXT>")) :
+                doc_text += line.replace("\n", " ")
+                line = f.readline()
+        elif (line.__contains__("</DOC>")) :
+            # Turn plaintext into tokens (this reduces memory usage by ~20%)
+            tokens = preprocessing(doc_text)
+            # Save the key value pair
+            doc_dic[doc_title] = tokens
+            # Reset values
+            doc_title = ""
+            doc_text = ""
 
-def preprocessing(str):
-    """This function is to remove the no character word in the text sunch as number and punctuation, also remove the stopwords and stem each word
-    parameter:a string to process
-    return: a list containing tokens which have been processed"""
+# Function to handle processing of plaintext into vocabulary
+def preprocessing(doc_text) :
+    # Tokenization
+    tokens = word_tokenize(doc_text)
 
-    tokenizer=RegexpTokenizer(r'[A-Za-z]+')
-    tokens=tokenizer.tokenize(str)
-    tokensWithoutStopwords=[token for token in tokens if token not in stopwords.words('english')]
-    stemmer=PorterStemmer()
-    stemmingTokens=[stemmer.stem(token) for token in tokensWithoutStopwords]
-    return stemmingTokens
+    # Stopword Removal
+    stop_words = set(stopwords.words("english"))
+    filtered_list = []
+    for word in tokens:
+        if word.casefold() not in stop_words:
+            filtered_list.append(word)
+
+    # Remove numbers and punctuation
+    filtered_list = [word.lower() for word in filtered_list if word.isalpha()]
+
+    # Stemming w/ Porter Stemmer
+    stemmer = PorterStemmer()
+    filtered_list = [stemmer.stem(word) for word in filtered_list]
+
+    # Return results
+    index.update(tokens)
+    return filtered_list
 
 def vectorSpaceModel(doc_dic):
     """this function is to create an inverted index, double dictionary is used, the key of vectorSpace is the token, the value of the vectorSpace
@@ -146,7 +154,10 @@ def createQueryVector(query,vectorModel):
     #calculate tf-idf and query length
     for queryTerm in queryTermWeights:
         #vectorModel[1] is the dictionary of idf,the key is the term in the index
-        weight=(queryTermWeights[queryTerm]/maxTf)*vectorModel[1][queryTerm]
+        if (queryTerm in vectorModel[1]):
+            weight=(queryTermWeights[queryTerm]/maxTf)*vectorModel[1][queryTerm]
+        else:
+            weight=0
         queryTermWeights[queryTerm]=weight
         queryLength+=weight**2
     queryLength=math.sqrt(queryLength)
@@ -202,25 +213,23 @@ def outputToFile(outfile, query_num,rankedList):
         formattedQueryRank=str(query_num)+" Q0 "+" "+doc_num+" "+str(doc_rank+1)+" "+doc_score+" "+"tag_name"
         outfile.write(formattedQueryRank+"\n")
 
-
 ########### main #############
 
-#get a list of all files in the coll/ directory
-lst_filenames=os.listdir("coll")
+# Get a list of all files in the coll/ directory
+lst_filenames = os.listdir("coll")
 
 #open each file one by one, separate the documents within file
 #process into tokens and save (document number, tokens) as key-value pair
 doc_dic={}
-count=0 #progress bar
-num_total=len(lst_filenames) #progress bar
-startTime=time.perf_counter() #progress bar
-for filename in lst_filenames:
-    file_path="coll\\"+filename
-    part_doc_dic=createTerms(file_path)
-    doc_dic=doc_dic | part_doc_dic
-    count+=1 #progress bar
-    print("Preprocessing:",count,"/",num_total,"documents") #progress bar
-endTime=time.perf_counter() #progress bar
+count = 0
+startTime = time.perf_counter()
+for filename in lst_filenames :
+    file_path =  "coll\\" + filename
+    f = open(file_path, "r")
+    separate_docs(f)
+    count+=1
+    print("Preprocessing:", count, "/", len(lst_filenames))
+endTime = time.perf_counter()
 print("Preprocessing elapsed time:",endTime-startTime,"seconds") #progress bar
 
 #create inverted index from documents and tokens
@@ -228,7 +237,6 @@ startTime=time.perf_counter() #progress bar
 vector=vectorSpaceModel(doc_dic)
 endTime=time.perf_counter() #progress bar
 print("Indexing elapsed time:",endTime-startTime,"seconds") #progress bar
-
 
 #process queries into tokens
 startTime=time.perf_counter() #progress bar
